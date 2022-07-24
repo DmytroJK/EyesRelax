99bfc0a63e01fe2573c130e1f9206c7c3b93b77f
Vasyl Kaminskyi <vasia1415@gmail.com>
2022-07-24 13:10:21 +0300

    add new library project

---
+++ Source/EyesRelax/Class1.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace EyesRelax
+{
+    public class Class1
+    {
+    }
+}
---
+++ Source/EyesRelax/Timer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace EyesRelaxUI
+{
+    internal class Timer
+    {
+        private static Timer aTimer;
+        
+        public static void Main()
+        {
+            aTimer = new System.Timers.Timer(1500000); //таймер на 25 хв
+        }
+
+        public static implicit operator Timer(System.Timers.Timer v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
